90bdb98c9051332a582c8a43000d1af75ee59cbb
IsaacCosta <isaactcosta20142@gmail.com>
2025-09-18 16:40:09 -0300

    update_class_carro

--- programacao/aulas_programacao_20250917/primeiro_codigo.py
+++ programacao/aulas_programacao_20250917/primeiro_codigo.py
@@ -64,7 +64,7 @@ class Carro:
     def __init__(self, modelo, placa):
         self.modelo = modelo
         self.placa = placa
-        self.disponivel = False
+        self.disponivel = True
 
     def __str__(self):
         status = "Disponível" if self.disponivel else "Indisponível"
