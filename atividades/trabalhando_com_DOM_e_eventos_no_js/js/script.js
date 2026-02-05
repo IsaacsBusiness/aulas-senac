9711024e7817b198acf7e66d919d23121304ac47
IsaacCosta <isaactcosta20142@gmail.com>
2026-02-04 22:28:12 -0300

    update_atividade_dom_js_20260204

--- atividades/trabalhando_com_DOM_e_eventos_no_js/js/script.js
+++ atividades/trabalhando_com_DOM_e_eventos_no_js/js/script.js
@@ -1,5 +1,6 @@
 let timeoutMensagem; // controle global
 
+// Função para mostrar mensagens temporárias
 function mostrarMensagem(texto) {
     const mensagem = document.getElementById("pMensagem");
 
@@ -15,32 +16,73 @@ function mostrarMensagem(texto) {
     }, 3000);
 }
 
+// Função para troca de páginas com base no usuário e senha
 function trocaPaginas() {
     let usuario = document.getElementById("usuario").value;
     let senha = document.getElementById("inpSenha").value;
 
-    // dropdown não selecionada
+    // Dropdown não selecionada
     if (usuario === "0") {
         mostrarMensagem("Selecione o tipo de usuário!");
     }
 
     // Vendedor
     else if (usuario === "1" && senha === "123") {
-        window.location.href = "vendas.html";
+        window.location.href = "vendas.php";
     }
 
     // Estoquista
     else if (usuario === "2" && senha === "456") {
-        window.location.href = "estoque.html";
+        window.location.href = "estoque.php";
     }
 
     // Admin
     else if (usuario === "3" && senha === "789") {
-        window.location.href = "admin.html";
+        window.location.href = "admin.php";
     }
 
     // erro geral
     else {
         mostrarMensagem("Usuário ou senha inválidos!");
     }
+}
+
+// Função para abrir o modal de exclusão
+function abrirModal(id) {
+    document.getElementById('modalExcluir').style.display = 'flex';
+    document.getElementById('linkExcluir').href =
+        'php-action/delete.php?id=' + id;
+}
+
+function fecharModal() {
+    document.getElementById('modalExcluir').style.display = 'none';
+}
+
+function confirmarExclusao(id) {
+    document.getElementById('linkExcluir').href =
+        'php-action/delete.php?id=' + id;
+
+    document.getElementById('modalExcluir').style.display = 'flex';
+}
+
+function fecharExcluir() {
+    document.getElementById('modalExcluir').style.display = 'none';
+}
+
+// Desaparecer mensagem automaticamente
+setTimeout(() => {
+    const msg = document.querySelector('.msg');
+    if (msg) {
+        msg.style.opacity = '0';
+        setTimeout(() => msg.remove(), 500);
+    }
+}, 5000); // desaparece após 5 segundos
+
+// Funções para o modal de sair
+function abrirModalSair() {
+    document.getElementById('modalSair').style.display = 'flex';
+}
+
+function fecharModalSair() {
+    document.getElementById('modalSair').style.display = 'none';
 }
